1f1471c6d75725178ee1584b63af91a5c04de88b
christine.pham <cstinepham@gmail.com>
2018-11-15 16:33:18 -0800

    Add logout button

--- music_app/app/controllers/application_controller.rb
+++ music_app/app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
+  helper_method :logged_in?
 
   def current_user
-    user = User.find_by_credentials(params[:user][:email], params[:user][:password])
-    user ? user : nil
+    @current_user ||= User.find_by(session_token: session[:session_token])
   end
 
   def logged_in?
@@ -12,5 +12,11 @@ class ApplicationController < ActionController::Base
 
   def log_in_user!(user)
     session[:session_token] = user.reset_session_token!
+  end
+
+  def log_out!
+    current_user.reset_session_token!
+    session[:session_token] = nil
+
   end
 end
--- music_app/app/controllers/sessions_controller.rb
+++ music_app/app/controllers/sessions_controller.rb
@@ -9,4 +9,9 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def destroy
+    log_out!
+    redirect_to new_session_url
+  end
+
 end
